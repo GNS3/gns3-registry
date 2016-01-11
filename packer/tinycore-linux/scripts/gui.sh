54854648d73d572b33268a60b3a48529770e2f70
Julien Duponchelle <julien@duponchelle.info>
2016-01-11 13:47:29 +0100

    Use Xorg for tinycore appliances
    This use the tips from @ehlers allowing a better sync
    between mouse pointer and VNC client.

--- packer/tinycore-linux/scripts/gui.sh
+++ packer/tinycore-linux/scripts/gui.sh
@@ -1,7 +1,21 @@
 # Install the GUI
 tce-load -wi fltk-1.3
 tce-load -wi flwm
-tce-load -wi Xvesa
+
+# Install gui
+tce-load -wi Xorg-7.7
+# set X resolution to 800x600
+cat > 99-resolution.conf <<'EOF'
+Section "Screen"
+    Identifier "Screen0"
+    DefaultDepth 24
+    SubSection "Display"
+        Modes "800x600"
+    EndSubSection
+EndSection
+EOF
+sudo mv 99-resolution.conf /usr/local/share/X11/xorg.conf.d/
+
 tce-load -wi wbar
 tce-load -wi Xprogs
 tce-load -wi Xlibs
